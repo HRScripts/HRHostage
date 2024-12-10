0e9e39944118a133d0c5401ac7f8bdb78dd58025
HR0110 <hrisiraya@abv.bg>
2024-12-10 19:03:35 +0200

    feat(fxmanifest.lua): repository description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ lua54 'yes'
 name 'HRHostage'
 author 'HRScripts Development'
 description 'Take Hostage Script'
+repository 'https://github.com/HRScripts/HRHostage'
 version '1.0.2'
 
 shared_script '@HRLib/import.lua'
